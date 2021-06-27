0f700ced221ae57ade5b7b0e741dd8bf1d518ff9
Anton Brink <twiggymocha@gmail.com>
2021-06-27 18:17:48 +0200

    Works on other screen sizes now, spin more suspensefull and added riot footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,13 +12,29 @@ export default function Home() {
   const [yourComp, setYourComp] = useState("")
   const [btnClass, setBtnClass] = useState("spinBtn")
   const [btnContainerClass, setBtnContainerClass] = useState("btnContainer")
+  const [timer, setTimer] = useState("done")
 
   const myfunction = () => {
-    const myRandomNumber = Math.floor(Math.random() * 4600) / 100
-    const newRandomNumber = Math.round(myRandomNumber) * 100
+    let myRandomNumber = Math.floor(Math.random() * 4600) / 100
+
+    console.log(window.innerWidth)
+
+    let pixelAdjust = 0
+
+    if (window.innerWidth < 1200) {
+      pixelAdjust = 30
+      console.log("occurs")
+    }
+
+    const spinnerWidth = window.innerWidth * 0.8
+    let spinnerAdjust = spinnerWidth / 2 / 100
+    spinnerAdjust = (Math.floor(spinnerAdjust) * 100) / 100
+    console.log(spinnerAdjust)
+
+    const newRandomNumber = Math.round(myRandomNumber) * 100 + pixelAdjust
     const randomNumWithPixels = "-" + newRandomNumber + "px"
     setSpinLeft(randomNumWithPixels)
-    const compId = Math.round((newRandomNumber / 100) % 27) + 7
+    const compId = Math.round((newRandomNumber / 100) % 27) + spinnerAdjust
 
     if (yourComp === divArray[compId]) {
       myfunction()
@@ -32,11 +48,13 @@ export default function Home() {
     setTimeout(function () {
       setBtnClass("spinBtn")
       setBtnContainerClass("btnContainer")
+      setTimer("notdone")
     }, 1100)
+    setTimer("done")
   }
 
   return (
-    <div>
+    <main className="Page">
       <Helmet>
         <title>tftrandom</title>
         <meta
@@ -49,45 +67,52 @@ export default function Home() {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
         ></script>
       </Helmet>
-
-      <div className="spinner">
-        <img src={pointerImage} className="pointer" alt=""></img>
-        <div className="spinItemsContainer">
-          {divArray2.map(comp => {
-            return (
-              <div
-                className="spinItem"
-                style={{
-                  left: spinLeft,
-                  backgroundImage: `radial-gradient(circle, ${comp.colors})`,
-                }}
-              >
-                <img src={comp.img} className="traitImg" alt="" />
-                <div className="traitText">
-                  {comp.name}({comp.compsize}){console.log(comp.img)}
+      <div className="content">
+        <div className="spinner">
+          <img src={pointerImage} className="pointer" alt=""></img>
+          <div className="spinItemsContainer">
+            {divArray2.map(comp => {
+              return (
+                <div
+                  className="spinItem"
+                  style={{
+                    left: spinLeft,
+                    backgroundImage: `radial-gradient(circle, ${comp.colors})`,
+                  }}
+                >
+                  <img src={comp.img} className="traitImg" alt="" />
+                  <div className="traitText">
+                    {comp.name}({comp.compsize})
+                  </div>
                 </div>
-              </div>
-            )
-          })}
+              )
+            })}
+          </div>
+        </div>
+
+        <h1 className="compReveal">
+          {yourComp.name && timer !== "done"
+            ? "Your Comp should include the " + yourComp.name + " trait"
+            : "Spin to get your random trait!"}
+        </h1>
+
+        <div className={btnContainerClass}>
+          <button
+            onClick={() => {
+              myfunction()
+            }}
+            className={btnClass}
+          >
+            Spin!
+          </button>
         </div>
       </div>
 
-      <h1 className="compReveal">
-        {yourComp.name
-          ? "Your Comp should include the " + yourComp.name + " trait"
-          : "Spin to get your random trait!"}
-      </h1>
-
-      <div className={btnContainerClass}>
-        <button
-          onClick={() => {
-            myfunction()
-          }}
-          className={btnClass}
-        >
-          Spin!
-        </button>
-      </div>
-    </div>
+      <footer className="footerClass">
+        TFTrandom was created under Riot Games' "Legal Jibber Jabber" policy
+        using assets owned by Riot Games. Riot Games does not endorse or sponsor
+        this project.
+      </footer>
+    </main>
   )
 }
