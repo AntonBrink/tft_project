d115eee36822f8e13f37f054ca8d86e12c140377
Anton Brink <twiggymocha@gmail.com>
2021-12-11 02:41:42 +0200

    added manifest and offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `TFTRandom`,
+        short_name: `TFTRandom`,
+        start_url: `/`,
+        background_color: `#15aff2`,
+        theme_color: `#15aff2`,
+        display: `standalone`,
+        icon: `static/favicon.ico`,
+      },
+    },
+  ],
 }
